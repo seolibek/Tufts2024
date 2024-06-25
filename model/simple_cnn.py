82f944b2693a0d57422261ab2d1f89d7b4ab7364
Lisa Baek <seoli@MacBook-Pro-2.local>
2024-06-25 13:41:11 -0400

    untrained cnn simple

--- model/simple_cnn.py
+++ model/simple_cnn.py
@@ -15,24 +15,31 @@ class SimpleCNN(nn.Module):
     def __init__(self, final_dims):
         super(SimpleCNN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=204, out_channels=128, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(128)
         self.conv2 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels= final_dims, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(83 * 86, 512)
-        self.fc2 = nn.Linear(512, 83*86)  # Reduced dimensionality
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=final_dims, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(final_dims)
+        
+        self.fc1 = nn.Linear(final_dims * 83 * 86, 512)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(512, 83 * 86)
         self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(2, 2)
 
     def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = self.relu(self.conv3(x))
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.maxpool(x)
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.maxpool(x)
+        x = self.relu(self.bn3(self.conv3(x)))
+        x = self.maxpool(x)
         x = x.view(x.size(0), -1)  # Flatten the tensor
         x = self.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
         x = x.view(x.size(0), 83, 86)  # Reshape to (83, 86)
-
         return x
-    def final_dims(self):
-        return self.final_dims
 
 
 def calculate_aligned_accuracy(ground_truth, cluster_labels):
