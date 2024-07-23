5449de3d4c015965f2af4bbf017c3c7eb2bbcb6e
Lisa Baek <seoli@MacBook-Pro-2.local>
2024-07-23 13:48:33 -0400

    expected behav w low samples, high t

--- lund/utils.py
+++ lund/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.spatial.distance import pdist, squareform
 from sklearn.neighbors import kneighbors_graph
 from scipy.sparse.csgraph import laplacian
-from scipy.sparse.linalg import eigs,eigsh
+from scipy.sparse.linalg import eigs
 from sklearn.neighbors import KernelDensity
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import svds
@@ -19,7 +19,7 @@ class GraphExtractor:
     If NEigs not included in Hyperparam structure, , = argmax(abs(diff(eigenvals))) 
     eigenvalues are included in graph structure.
     '''
-    def __init__(self, sigma = 1.0, DiffusionNN = 100, NEigs = 1000):
+    def __init__(self, sigma = 1.0, DiffusionNN = 10, NEigs = 100):
         self.sigma = sigma
         self.DiffusionNN = DiffusionNN
         self.NEigs = int(NEigs)
@@ -28,75 +28,9 @@ class GraphExtractor:
 
     def extract_graph(self, X, Dist = None):
         n = len(X)
-        if Dist == None:
-            Dist = pdist(X)
-            Dist = squareform(Dist)
-        W = np.zeros((n, n))
-        P = np.zeros((n, n))
-        D = np.zeros((n, n))
-        for i in range(n):
-            idx = np.argpartition(Dist[i, :], self.DiffusionNN + 1)[:self.DiffusionNN + 1]
-            D_sorted = Dist[i, idx]
-            sorting = np.argsort(D_sorted)
-            idx = idx[sorting]
-
-            W[i, idx[1:]] = np.exp(-(D_sorted[1:] ** 2) / (self.sigma ** 2))
-            D[i, i] = np.sum(W[i, :])
-            P[i, idx[1:]] = W[i, idx[1:]] / D[i, i]
-
-    # Calculate pi, stationary distribution
-        pi = np.diag(D) / np.sum(np.diag(D))
-
-    # Calculate the eigendecomposition of P
-        try:
-            if self.NEigs:
-                n_eigs = self.NEigs
-                eigvals, eigvecs = eigsh(P, k=n_eigs, which='LM')
-                eigvals = np.sort(np.abs(eigvals))[::-1]
-            else:
-                eigvals, eigvecs = eigsh(P, k=20, which='LM')
-                eigvals = np.sort(np.abs(eigvals))[::-1]
-                n_eigs = np.argmax(np.abs(np.diff(eigvals))) + 1
-
-                # Take care of fringe cases where we don't gather enough eigenvectors
-                if n_eigs < 5:
-                    n_eigs = 5
-
-            eigvecs = np.real(eigvecs[:, :n_eigs])
-            eigvals = eigvals[:n_eigs]
-
-            # Set theoretical value for first eigenpair
-            eigvecs[:, 0] = 1
-            eigvals[0] = 1
-
-            # Store in graph structure
-            Graph = {
-                'Hyperparameters': {
-                    'Sigma': self.sigma,
-                    'DiffusionNN': self.DiffusionNN
-                },
-                'EigenVecs': eigvecs,
-                'EigenVals': eigvals,
-                'StationaryDist': pi,
-                'P': P,
-                'W': W
-            }
-
-        except Exception as e:
-            print('EigenDecomposition of P failed:', e)
-            Graph = {
-                'Hyperparameters': {
-                    'Sigma': self.sigma,
-                    'DiffusionNN': self.DiffusionNN
-                },
-                'EigenVecs': np.nan,
-                'EigenVals': np.nan,
-                'StationaryDist': np.nan,
-                'P': np.nan,
-                'W': np.nan
-            }
-
-        return Graph
+        # if Dist == None:
+        #     Dist = pdist(X)
+        #     Dist = squareform(Dist)
  
         # W = np.zeros((n,n))
         # P = np.zeros((n,n))
@@ -115,64 +49,81 @@ class GraphExtractor:
 
             
         # pi = np.diag(D) / np.sum(np.diag(D))
+        Dist = squareform(pdist(X))
+        W = np.zeros((n, n))
+        P = np.zeros((n, n))
+        D = np.zeros(n)  
 
-        # #ok do the eigendecomp here..
-        # print('entering try')
-        # try:
+        for i in range(n):
+            idx = np.argpartition(Dist[i, :], self.DiffusionNN + 1)[:self.DiffusionNN + 1]
+            D_sorted = Dist[i, idx]
+            sorting = np.argsort(D_sorted)
+            idx = idx[sorting]
+
+            W[i, idx[1:]] = np.exp(-(D_sorted[1:] ** 2) / (self.sigma ** 2))
             
-        #     if self.NEigs is not None:
-        #         n_eigs = min(self.NEigs, n)
-        #         eigvals, eigvecs = eigs(P, k = n_eigs) 
-        #         eigvals = np.real(eigvals)
-        #         sorted_eigvals = np.sort(-np.abs(eigvals))
-        #         eiggap = np.abs(np.diff(sorted_eigvals)) 
+            D[i] = np.sum(W[i, :])
+
+            if D[i] > 0:  
+                P[i, idx[1:]] = W[i, idx[1:]] / D[i]
+
+        pi = D / np.sum(D)
+
+        #ok do the eigendecomp here..
+        try:
+            
+            if self.NEigs is not None:
+                n_eigs = min(self.NEigs, n)
+                eigvals, eigvecs = eigs(P, k = n_eigs) 
+                eigvals = np.real(eigvals)
+                sorted_eigvals = np.sort(-np.abs(eigvals))
+                eiggap = np.abs(np.diff(sorted_eigvals)) 
                 
-        #         # pass
-        #     else:
-        #         print('else condition of try executed')
-        #         eigvals, eigvecs = eigs(P, k=15) #scipy order is different (see docs if needed)
-        #         eigvals = np.real(eigvals)
-        #         sorted_eigvals = np.sort(-np.abs(eigvals))
-        #         eiggap = np.abs(np.diff(sorted_eigvals))
-        #         n_eigs = np.argmax(eiggap) + 1 #python indexing is 0.
-        #         if n_eigs < 5:
-        #             n_eigs = 5
-        #     idx = np.argsort(-np.abs(eigvals))
-        #     eigvals = eigvals[idx][:n_eigs]
-        #     eigvecs = eigvecs[:, idx][:n_eigs]
+                # pass
+            else:
+                eigvals, eigvecs = eigs(P, k=15) #scipy order is different (see docs if needed)
+                eigvals = np.real(eigvals)
+                sorted_eigvals = np.sort(-np.abs(eigvals))
+                eiggap = np.abs(np.diff(sorted_eigvals))
+                n_eigs = np.argmax(eiggap) + 1 #python indexing is 0.
+                if n_eigs < 5:
+                    n_eigs = 5
+
+            idx = np.argsort(-np.abs(eigvals))
+            eigvals = eigvals[idx][:n_eigs]
+            eigvecs = eigvecs[:, idx][:n_eigs]
 
             
-        #     # setting theoretical val for first eigenpair
-        #     eigvecs[:, 0] = 1
-        #     eigvals[0] = 1
-        #     print('sucessfully')
-        #     #store in graph structure?
-        #     graph = {
-        #         'Hyperparameters': {
-        #             'Sigma': self.sigma,
-        #             'DiffusionNN': self.DiffusionNN
-        #         },
-        #         'EigenVecs': np.real(eigvecs),
-        #         'EigenVals': np.real(eigvals),
-        #         'StationaryDist': pi,
-        #         'P': P,
-        #         'W': W
-        #     }
-        # except Exception as e:  
-        #     print('EigenDecomposition of P failed')
-        #     graph = {
-        #         'Hyperparameters': {
-        #             'Sigma': self.sigma,
-        #             'DiffusionNN': self.DiffusionNN
-        #         },
-        #         'EigenVecs': np.nan,
-        #         'EigenVals': np.nan,
-        #         'StationaryDist': np.nan,
-        #         'P': np.nan,
-        #         'W': np.nan
-        #     }
+            # setting theoretical val for first eigenpair
+            eigvecs[:, 0] = 1
+            eigvals[0] = 1
+            #store in graph structure?
+            graph = {
+                'Hyperparameters': {
+                    'Sigma': self.sigma,
+                    'DiffusionNN': self.DiffusionNN
+                },
+                'EigenVecs': np.real(eigvecs),
+                'EigenVals': np.real(eigvals),
+                'StationaryDist': pi,
+                'P': P,
+                'W': W
+            }
+        except Exception as e:  
+            print('EigenDecomposition of P failed')
+            graph = {
+                'Hyperparameters': {
+                    'Sigma': self.sigma,
+                    'DiffusionNN': self.DiffusionNN
+                },
+                'EigenVecs': np.nan,
+                'EigenVals': np.nan,
+                'StationaryDist': np.nan,
+                'P': np.nan,
+                'W': np.nan
+            }
         
-        # return graph
+        return graph
     
     #code taken from Kabir's implementation of LAND components, in python.
 
