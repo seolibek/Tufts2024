d717ff119cb733755f03aca01ad9e6a357f440e7
aryamantepal <aryamandora@gmail.com>
2024-08-06 12:31:35 -0400

    included the link

--- matlabfiles/lle.m
+++ matlabfiles/lle.m
@@ -1,4 +1,5 @@
 % LLE ALGORITHM (using K nearest neighbors)
+% CREDITS: https://cs.nyu.edu/~roweis/lle/code.html
 %
 % [Y] = lle(X,K,dmax)
 %
@@ -15,7 +16,7 @@ function [Y] = lle(X,K,d)
 fprintf(1,'LLE running on %d points in %d dimensions\n',N,D);
 
 
-% STEP1: COMPUTE PAIRWISE DISTANCES & FIND NEIGHBORS 
+% STEP1: COMPUTE PAIRWISE DISTANCES & FIND NEIGHBORS
 fprintf(1,'-->Finding %d nearest neighbours.\n',K);
 
 X2 = sum(X.^2,1);
@@ -29,8 +30,8 @@ neighborhood = index(2:(1+K),:);
 % STEP2: SOLVE FOR RECONSTRUCTION WEIGHTS
 fprintf(1,'-->Solving for reconstruction weights.\n');
 
-if(K>D) 
-  fprintf(1,'   [note: K>D; regularization will be used]\n'); 
+if(K>D)
+  fprintf(1,'   [note: K>D; regularization will be used]\n');
   tol=1e-3; % regularlizer in case constrained fits are ill conditioned
 else
   tol=0;
@@ -38,11 +39,11 @@ end
 
 W = zeros(K,N);
 for ii=1:N
-   z = X(:,neighborhood(:,ii))-repmat(X(:,ii),1,K); % shift ith pt to origin
-   C = z'*z;                                        % local covariance
-   C = C + eye(K,K)*tol*trace(C);                   % regularlization (K>D)
-   W(:,ii) = C\ones(K,1);                           % solve Cw=1
-   W(:,ii) = W(:,ii)/sum(W(:,ii));                  % enforce sum(w)=1
+  z = X(:,neighborhood(:,ii))-repmat(X(:,ii),1,K); % shift ith pt to origin
+  C = z'*z;                                        % local covariance
+  C = C + eye(K,K)*tol*trace(C);                   % regularlization (K>D)
+  W(:,ii) = C\ones(K,1);                           % solve Cw=1
+  W(:,ii) = W(:,ii)/sum(W(:,ii));                  % enforce sum(w)=1
 end;
 
 
@@ -50,17 +51,17 @@ end;
 fprintf(1,'-->Computing embedding.\n');
 
 % M=eye(N,N); % use a sparse matrix with storage for 4KN nonzero elements
-M = sparse(1:N,1:N,ones(1,N),N,N,4*K*N); 
+M = sparse(1:N,1:N,ones(1,N),N,N,4*K*N);
 for ii=1:N
-   w = W(:,ii);
-   jj = neighborhood(:,ii);
-   M(ii,jj) = M(ii,jj) - w';
-   M(jj,ii) = M(jj,ii) - w;
-   M(jj,jj) = M(jj,jj) + w*w';
+  w = W(:,ii);
+  jj = neighborhood(:,ii);
+  M(ii,jj) = M(ii,jj) - w';
+  M(jj,ii) = M(jj,ii) - w;
+  M(jj,jj) = M(jj,jj) + w*w';
 end;
 
 % CALCULATION OF EMBEDDING
-options.disp = 0; options.isreal = 1; options.issym = 1; 
+options.disp = 0; options.isreal = 1; options.issym = 1;
 [Y,eigenvals] = eigs(M,d+1,0,options);
 Y = Y(:,2:d+1)'*sqrt(N); % bottom evect is [1,1,1,1...] with eval 0
 
